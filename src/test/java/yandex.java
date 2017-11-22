16f3407d9018bd6c6914210ec000522900466bc7
E.Voronova <light-77@bk.ru>
2017-11-23 00:03:09 +0300

    Оптимизация_поиска

--- src/test/java/yandex.java
+++ src/test/java/yandex.java
@@ -34,20 +34,19 @@ public class yandex {
   public void yandexMarket() throws InterruptedException {
     wd.manage().window().maximize();
     wd.get("https://yandex.ru/"); //2. Зайти на yandex.ru
-    wd.findElement(By.xpath("html/body/div[1]/div[3]/div[2]/div[2]/div/div[2]/div/div[1]/div/a[5]")).click(); //3.	Перейти на Яндекс Маркет
+    wd.findElement(By.xpath(".//a[5]")).click(); //3.	Перейти на Яндекс Маркет
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.findElement(By.xpath("html/body/div[1]/div[2]/noindex/ul/li[2]/a")).click(); //4.	Выбрать раздел  Компьютеры
-    wd.findElement(By.xpath("html/body/div[1]/div[4]/div[1]/div/div[1]/div/a[1]")).click(); //5.	 Выбрать раздел Планшеты
+wd.findElement(By.xpath(".//div[1]//li[2]/a")).click(); //4.	Выбрать раздел  Компьютеры html/body/div[1]/div[2]/noindex/ul/li[2]/a
+    wd.findElement(By.xpath(".//div[4]/div[1]/div/div[1]/div/a[1]")).click(); //5.	 Выбрать раздел Планшеты
     // прокрутить страницу вниз и подождать
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     JavascriptExecutor jse = (JavascriptExecutor) wd;
     jse.executeScript("window.scrollBy(0,100000)", "");
-    wd.findElementByXPath("html/body/div[1]/div[4]/div[2]/div[2]/div[2]/div/div[30]/div[2]/a"); //6.	Зайти в расширенный поиск
-    if (!wd.findElement(By.xpath("html/body/div[1]/div[4]/div[2]/div[2]/div[2]/div/div[30]/div[2]/a")).isSelected()) {
-      wd.findElement(By.xpath("html/body/div[1]/div[4]/div[2]/div[2]/div[2]/div/div[30]/div[2]/a")).click();
+    wd.findElementByXPath(".//div[1]//div[30]/div[2]/a"); //6.	Зайти в расширенный поиск
+    if (!wd.findElement(By.xpath(".//div[1]//div[30]/div[2]/a")).isSelected()) {
+      wd.findElement(By.xpath(".//div[1]//div[30]/div[2]/a")).click();
     }
     jse.executeScript("window.scrollBy(0,250)", "");
-    //Todo добавить ожидание загрузки элемента
     wd.findElement(By.id("glf-pricefrom-var")).clear();
     // 7.	Задать параметр поиска от 20000 до 25000 рублей.
     wd.findElement(By.id("glf-pricefrom-var")).sendKeys("20000");
@@ -55,24 +54,23 @@ public class yandex {
     // подождать выборку
     wd.findElement(By.id("glf-priceto-var")).clear();
     wd.findElement(By.id("glf-priceto-var")).sendKeys("25000");
-    wd.findElement(By.xpath("html/body/div[1]/div[4]/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/button")).click();// 8.	Выбрать производителя Acer
-    wd.findElement(By.xpath("html/body/div[1]/div[4]/div/div[1]/div[1]/div[2]/div[2]/div/span/span/input")).clear();
-    wd.findElement(By.xpath("html/body/div[1]/div[4]/div/div[1]/div[1]/div[2]/div[2]/div/span/span/input")).sendKeys("Acer");
-    wd.findElementByXPath("html/body/div[1]/div[4]/div/div[1]/div[1]/div[2]/div[2]/div/div[1]/div/div[1]/span/label");
-    if (!wd.findElement(By.xpath("html/body/div[1]/div[4]/div/div[1]/div[1]/div[2]/div[2]/div/div[1]/div/div[1]/span/label")).isSelected()) {
-      wd.findElement(By.xpath("html/body/div[1]/div[4]/div/div[1]/div[1]/div[2]/div[2]/div/div[1]/div/div[1]/span/label")).click();
+    wd.findElement(By.xpath(".//div[2]/div[2]/div/div[2]/button")).click();// 8.	Выбрать производителя Acer
+    wd.findElement(By.xpath(".//div[2]/div[2]/div/span/span/input")).clear();
+    wd.findElement(By.xpath(".//div[2]/div[2]/div/span/span/input")).sendKeys("Acer");
+    wd.findElementByXPath(".//div[1]//div[2]/div/div[1]/div/div[1]/span/label"); //
+    if (!wd.findElement(By.xpath(".//div[1]//div[2]/div/div[1]/div/div[1]/span/label")).isSelected()) {
+      wd.findElement(By.xpath(".//div[1]//div[2]/div/div[1]/div/div[1]/span/label")).click();
     }
-    // прокрутить страницу вниз и подождать
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     jse.executeScript("window.scrollBy(0,100000)", "");
-    wd.findElement(By.xpath("html/body/div[1]/div[4]/div/div[1]/div[4]/a[2]")).click(); //9.	Применить условия поиска
-    String fr = wd.findElement(By.xpath("html/body/div[1]/div[4]/div[2]/div[1]/div[2]/div/div[1]/div[2]/div[4]/div[1]/div/a")).getText();//10.	Запомнить второй элемент в результатах поиска
+    wd.findElement(By.xpath(".//div[1]//div[4]/a[2]")).click(); //9.	Применить условия поиска
+    String fr = wd.findElement(By.xpath(".//div[1]//div[2]/div[4]/div[1]/div/a")).getText();//10.	Запомнить второй элемент в результатах поиска
     System.out.println(fr);
     wd.get("https://market.yandex.ru/"); //11.	В поисковую строку ввести запомненное значение.
     wd.findElement(By.id("header-search")).clear();
     wd.findElement(By.id("header-search")).sendKeys("\"" + fr + "\"");
-    wd.findElement(By.xpath("html/body/div[1]/div[1]/noindex/div/div/div[2]/div/div[1]/form/span[2]/button")).click();//12.	Найти и проверить, что наименование товара соответствует запомненному значению.
-    String fr2 = wd.findElement(By.xpath("html/body/div[1]/div[4]/div[2]/div[1]/div[2]/div/div[1]/div/div[4]/div[1]/div/a")).getText();
+    wd.findElement(By.xpath(".//button")).click();//12.	Найти и проверить, что наименование товара соответствует запомненному значению.
+    String fr2 = wd.findElement(By.xpath(".//div[1]//div[4]/div[1]/div/a")).getText();
     Assert.assertEquals(fr, fr2);
   }
 
